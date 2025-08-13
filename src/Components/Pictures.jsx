a6436a2dd6065c301766b3f2e518791c2e096322
shareefuddeen <140364218+shareefuddeen@users.noreply.github.com>
2025-08-12 12:43:19 -1200

    Youtube Video

--- src/Components/Pictures.jsx
+++ src/Components/Pictures.jsx
@@ -5,7 +5,7 @@ export const Pictures = ({ images, heading }) => {
 		<section>
 			<h1 className="text-center font-bold text-4xl mt-20">{heading}</h1>
 			<div className="grid grid-cols-3 grid-rows-3 gap-4 max-w-6xl mx-auto p-4">
-				{images.map((img, index) => (
+							{images.map((img, index) => (
 					<div
 						key={index}
 						className={`overflow-hidden rounded-2xl ${img.span}`}
@@ -17,6 +17,8 @@ export const Pictures = ({ images, heading }) => {
 						/>
 					</div>
 				))}
+										<iframe width="560" height="315" src="https://www.youtube.com/embed/k3I_A-e6PFk?si=htkGaaOwKVX-A0Zx" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
 			</div>
 		</section>
 	);
